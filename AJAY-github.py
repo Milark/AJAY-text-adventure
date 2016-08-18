e3794ab87ad37a14a0516252aa5279d898c7b4a9
Milan <milan.schouten@hotmail.com>
2016-08-18 12:49:00 +0200

    SpaceShip test
    added a lore and goal in the game
    implement or not?
    see AJAY_Lore for the lore

--- AJAY-github.py
+++ AJAY-github.py
@@ -1,45 +1,45 @@
 __author__ = 'milanschouten'
 
-import time # enabels the use of time commands
-import sys # enables sys.exit command
+import time  # enabels the use of time commands
+import sys  # enables sys.exit command
 
-def calc(): # A simple Calculator
+
+def calc():  # A simple Calculator
     time.sleep(0.75)
-    print ("Welcome To The Calculator")
+    print("Welcome To The Calculator")
     num_1 = int(input("- "))
     num_2 = int(input("- "))
-    print (num_1 + num_2)
+    print(num_1 + num_2)
     time.sleep(0.5)
     calc2()
 
+
 def calc2():
-    print ("do another calculation \"again\" or go back?")
+    print("do another calculation \"again\" or go back?")
     cq_1 = input("- ")
     if cq_1 == "again":
         calc()
     elif cq_1 == "back":
         choose()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(0.5)
         calc2()
 
-def prgrm(): # a small introduction to the program
-    print ("Welcome to \"TheProgram\"")
+
+def prgrm():  # a small introduction to the program
+    print("Welcome to \"TheProgram\"")
     time.sleep(1)
-    print ("What would you like your username to be??")
-    q_2 = input("- ")
-    time.sleep(0.75)
-    print ("Hello " + q_2)
-    time.sleep(0.75)
     choose()
 
-def choose(): #asks the user what fuction he/she wants to use
-    print ("What do you want to do? calc, game or exit")
+
+def choose():  # asks the user what fuction he/she wants to use
+    print("What do you want to do? calc, game or exit")
     choose_2()
-    
-def choose_2():    
-    q_3 = input ("- ")
+
+
+def choose_2():
+    q_3 = input("- ")
     if q_3 == "calc":
         calc()
     elif q_3 == "exit":
@@ -47,96 +47,106 @@ def choose_2():
     elif q_3 == "game":
         game_1()
     else:
-        print ("invalid input")
+        print("invalid input")
         time.sleep(1)
         choose_2()
 
-def main(): # The Main Function
-    print ("Hello user welcome to \"theProgram\"")
+
+def main():  # The Main Function
+    print("Hello user welcome to \"theProgram\"")
     time.sleep(1)
-    print ("Do you want to start? y/n")
+    print("Do you want to start? y/n")
     q_1 = input("- ")
     if q_1 == "y":
         prgrm()
     elif q_1 == "n":
-        print ("Okay bye!")
+        print("Okay bye!")
         time.sleep(1)
-        print ("exiting TheProgram...")
+        print("exiting TheProgram...")
         time.sleep(0.5)
         sys.exit
     else:
-        print ("Thats Not A valid Input")
-        print ("You Should Enter \"y\" or \"n\"")
+        print("Thats Not A valid Input")
+        print("You Should Enter \"y\" or \"n\"")
         time.sleep(0.5)
         main()
 
+
 def game_1():
-    print ("welcome to \"A journey Awaits you...\"")
+    print("welcome to \"A journey Awaits you...\"")
     time.sleep(0.75)
-    print ("if you want to play press y if you want to go back type \"back\"")
+    print("if you want to play press y if you want to go back type \"back\"")
     game_1choise()
-    
+
     gq_1 = input("- ")
     if gq_1 == "y":
         game_2()
     elif gq_1 == "back":
         choose()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(1)
         game_1choise()
 
 
 def game_2():
-    print ("lets start")
     time.sleep(1)
-    print ("you have to go on an adventure!, lets begin...")
+    print("lets begin, Aaron")
     time.sleep(1)
-    print ("you wake up in the middle of an grass field, you see a forest and a mountain")
+    print("you wake up knowing nothing about where you are and what you are")
     time.sleep(1)
-    print ("walk to the mountain: \"mountain\" or explore the forest:\"forest\"?")
+    print("the only thing you know is that you have to get back to where you don't know...")
+    time.sleep(1)
+    print("but sheer instinct tells you that you need start walking")
+    time.sleep(1)
+    print("walk to the \"mountain\" in the distance or explore the \"forest\" nearby")
     game_2choise()
-    
-def game_2choise():    
+
+def game_2choise():
     gq_2 = input("- ")
     if gq_2 == "forest":
         forest()
     elif gq_2 == "mountain":
         mountain()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(1)
         game_2choise()
 
+
 def mountain():
-    print ("you decided to walk to the mountain...")
+    print("you walked to the mountain it was a though walk but you made it...")
     time.sleep(1)
-    print ("after ours of walking you finally arrive at the mountain")
+    print("after ours of walking you finally arrive at the mountain")
     time.sleep(1)
-    print ("you see a cave entrance:\"cave\" but also a light coming from the top of the mountain:\"top\"")
+    print("you see a cave entrance:\"cave\" but also a light coming from the top of the mountain:\"top\"")
     mountainchoise()
-    
-def mountainchoise():    
+
+
+def mountainchoise():
     gq_4 = input("- ")
     if gq_4 == "cave":
         cave()
     elif gq_4 == "top":
         mountaintop()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(1)
         mountainchoise()
-    
+
+
 def mountaintop():
-    print ("yuo begin your journey to the top...")
+    print("yuo begin your journey to the top...")
     time.sleep(0.5)
 
+
 def cave():
-    print ("you wander around a bit ")
+    print("you wander around a bit ")
     time.sleep(0.5)
-    print ("but after a while you get sleepy")
+    print("but after a while you get sleepy")
     time.sleep(0.5)
     cave_2()
+
 
 def cave_2():
     print("you can \"explore\" further or get some \"sleep\"")
@@ -146,67 +156,73 @@ def cave_2():
     elif gq_9 == "sleep":
         cave_3()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(0.5)
         cave_2()
 
+
 def cave_3():
-    print ("You find a good spot to sleep and decide to call it a day...")
+    print("You find a good spot to sleep and decide to call it a day...")
     time.sleep(0.5)
-    print ("its about 9AM you slowly wake up")
+    print("its about 9AM you slowly wake up")
     time.sleep(0.5)
-    print ("you look at your hand and see that you are in the corner of a room")
+    print("you look at your hand and see that you are in the corner of a room")
     time.sleep(0.5)
-    print ("your hands are tied together with ducktape")
+    print("your hands are tied together with duct tape")
+
 
 def deepcave():
-    print ("WIP")
+    print("WIP")
+
 
 def forest():
-    print ("you chose to explore the forest")
+    print("you chose to explore the forest")
     time.sleep(1)
-    print ("as you walk you get a feeling that your being followed...")
+    print("as you walk you get a feeling that your being followed...")
     time.sleep(1)
-    print ("you see a little cabin in the distance is seems there's light, enter the cabin:\"y/n\"?")
+    print("you see a little cabin in the distance is seems there's light, enter the cabin:\"y/n\"?")
     forestchoise()
-    
-def forestchoise():    
+
+
+def forestchoise():
     gq_3 = input("- ")
     if gq_3 == "y":
         cabin()
     elif gq_3 == "n":
         DeepForest()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(1)
         forestchoise()
-        
+
+
 def deepforest():
-    print ("you walk deep into the forest, deeper and deeper and deeper...")
+    print("you walk deep into the forest, deeper and deeper and deeper...")
     time.sleep(0.5)
-    print ("after ours of walking you think \"i should really get some sleep\"")
+    print("after ours of walking you think \"i should really get some sleep\"")
     time.sleep(0.5)
-    print ("you decide to try and find shelter")
+    print("you decide to try and find shelter")
     time.sleep(0.5)
 
 
 def cabin():
-    print ("you open the door, there is a fire in the fireplace but no one is home")
+    print("you open the door, there is a fire in the fireplace but no one is home")
     time.sleep(1)
-    print ("you decide to eat and get some rest")
+    print("you decide to eat and get some rest")
     time.sleep(1)
-    print ("you've eaten and are very tired you go to bed...")
+    print("you've eaten and are very tired you go to bed...")
     time.sleep(1)
-    print ("right as you fall asleep you hear a loud knock on the door, \"BAM BAM BAM\"")
+    print("right as you fall asleep you hear a loud knock on the door, \"BAM BAM BAM\"")
     time.sleep(1)
-    print ("confused and scared you awnser: \"y...yes\"")
+    print("confused and scared you awnser: \"y...yes\"")
     time.sleep(1)
-    print ("a loud but rather deep voice awnsers:GET OUT OF THE HOUSE OR WE WILL BLOW IT UP!")
+    print("a loud but rather deep voice awnsers:GET OUT OF THE HOUSE OR WE WILL BLOW IT UP!")
     time.sleep(1)
-    print ("you realise you don't have alot of time nor choises what should you do?")
+    print("you realise you don't have alot of time nor choises what should you do?")
     time.sleep(1)
-    print ("open the suspicious looking trap door on the ground:\"open trap door\" or obey the loud man:\"obey man\"")
+    print("open the suspicious looking trap door on the ground:\"open trap door\" or obey the loud man:\"obey man\"")
     traporobey()
+
 
 def traporobey():
     gq_5 = input("- ")
@@ -215,43 +231,47 @@ def traporobey():
     elif gq_5 == "obey man":
         end_1()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(1)
         traporobey()
 
+
 def end_1():
-    print ("you decide to obey the man, you open the door")
+    print("you decide to obey the man, you open the door")
     time.sleep(1)
-    print ("you hear a loud BENG and you feel steel touching your head, but as you try to respond everything goes black...")
+    print("you hear a loud BENG and you feel steel touching your head, but as you try to respond everything goes black...")
     time.sleep(1)
-    print ("you died, try being a bit more adventurous next time")
+    print("you hear a womens voice whispering in your ear \"Aaron, Aaron! you have to try again...\"")
     time.sleep(1)
     aorb()
 
+
 def aorb():
-    print ("go \"back\" or try \"again\"")
+    print("go \"back\" or try \"again\"")
     gq_6 = input("- ")
     if gq_6 == "again":
         game_2()
     elif gq_6 == "back":
         choose()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(1)
         aorb()
 
+
 def basement():
-    print ("you fall 2 meters and land on your knee, you think your leg is broken you need medical attention fast")
+    print("you fall 2 meters and land on your knee, you think your leg is broken you need medical attention fast")
     time.sleep(0.5)
-    print ("you call for help, to your suprise someone awnsers...")
+    print("you call for help, to your suprise someone awnsers...")
     time.sleep(0.5)
-    print ("you're not supposed to be here...")
+    print("you're not supposed to be here Aaron...")
     time.sleep(0.5)
-    print ("frightend to your core you realise you dont have alot of time")
+    print("how does he know my name?!?!")
     time.sleep(0.5)
-    print ("scream \"help\" or try to \"crawl\" away...")
+    print("scream \"help\" or try to \"crawl\" away...")
     basementchoise()
-    
+
+
 def basementchoise():
     gq_7 = input("- ")
     if gq_7 == "help":
@@ -259,23 +279,29 @@ def basementchoise():
     elif gq_7 == "crawl":
         basement4()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(1)
         basementchoise()
 
+
 def basement2():
-    print ("the man aproaches you slowly you think he means no harm...")
+    print("the man aproaches you slowly you think he means no harm...")
     time.sleep(0.5)
-    print ("until you you see his fist moving towards your face, you try to duck but its too late everything goes black")
+    print("until you you see his fist moving towards your face, you try to duck but its too late everything goes black")
     time.sleep(5)
-    print ("no... this is not the end")
+    print("no... this is not the end")
     time.sleep(0.5)
-    print ("you gently open your eyes only to be blinded with a bright light")
+    print("you gently open your eyes only to be blinded with a bright light")
     time.sleep(0.5)
-    print ("as you get used to the light you realise your in a cage...")
+    print("as you get used to the light you realise your in a cage...")
     time.sleep(0.5)
-    print ("you have two options \"scream\" or \"accept\" your fate...")
+    print("you see a sign it sais: \"SEA members captured: 1...")
+    time.sleep(0.5)
+    print("...{SEA stands for: SpaceExplorationAgency, you are apart of SEA}...")
+    time.sleep(0.5)
+    print("you have two options \"scream\" or \"accept\" your fate...")
     basement2choise()
+
 
 def basement2choise():
     gq_8 = input("- ")
@@ -288,39 +314,47 @@ def basement2choise():
         time.sleep(0.5)
         basement2choise()
 
+
 def end_2():
     print("after ours or days or month of waiting you slowly realise...")
     time.sleep(0.5)
     print("t... this")
     time.sleep(0.5)
-    print ("is the end")
+    print("is the end")
     time.sleep(0.5)
-    print ("your sense of time and reality are gone")
+    print("your sense of time and reality are gone")
     time.sleep(0.5)
-    print ("slowly everything fades away")
+    print("slowly everything fades away")
     time.sleep(3)
-    print ("goodbye...")
+    print("goodbye...")
     time.sleep(3)
     choose()
 
+
 def basement3():
-    print("WIP")
+    print("you scream for help it doesn't seem to help")
+    time.sleep(0.5)
+    print("after a while you give up")
+    time.sleep(0.5)
+    print("to your suprise somepne enters the room filled with caches")
+    time.sleep(0.5)
 
 def basement4():
-    print ("you try to crawl away, but your almost certant its a losthope")
-    print ("until the man trips and falls, you think \"this is my chance\"")
-    print ("you open the door and lock it, but the lock looks rather fragile")
-    print ("you hear the man struggling outside")
-    print ("there are alot of things inside the rectangle shaped room")
-    print ("a pile of wooden boards, they seem to fit in the hooks next to the door")
-    print ("a medkit, for your broken leg")
-    print ("you also see a red door")
-    print ("these are your choises:")
-    print ("1. \"barricade\" the \"door\"")
-    print ("2. \"enter\" the red \"door\"")
-    print ("3. \"use\" the \"medkit\" for your leg")
-    print ("4. go back outside to \"finish\" the \"man\" now he's still in shock")
+    print("you try to crawl away, but your almost certant its a losthope")
+    print("until the man trips and falls, you think \"this is my chance\"")
+    print("you open the door and lock it, but the lock looks rather fragile")
+    print("you hear the man struggling outside")
+    print("there are alot of things inside the rectangle shaped room")
+    print("a pile of wooden boards, they seem to fit in the hooks next to the door")
+    print("a medkit, for your broken leg")
+    print("you also see a red door")
+    print("these are your choises:")
+    print("1. \"barricade\" the \"door\"")
+    print("2. \"enter\" the red \"door\"")
+    print("3. \"use\" the \"medkit\" for your leg")
+    print("4. go back outside to \"finish\" the \"man\" now he's still in shock")
     basement4choise()
+
 
 def basement4choise():
     gq_10 = input("- ")
@@ -335,35 +369,38 @@ def basement4choise():
     elif gq_10 == "secret":
         dream_ending()
     else:
-        print ("invalid input..")
+        print("invalid input..")
         time.sleep(1)
         basement4choise()
-           
+
+
 def dream_ending():
-    print ("you hear a voice calling you...")
+    print("you hear a voice calling you...")
     time.sleep(0.5)
-    print ("you slowly open your eyes, a women looking around her 50's greets you")
+    print("you slowly open your eyes, a women looking around her 50's greets you")
     time.sleep(0.5)
-    print ("it's your mom...")
+    print("it's your mom...")
     time.sleep(0.5)
-    print ("you think to yourself: \"that was one hell of a dream\"")
+    print("you think to yourself: \"that was one hell of a dream\"")
     time.sleep(5)
     aorb()
-           
+
+
 def basement5():
-    print ("you see a ladder running up to a vault style looking door")
+    print("you see a ladder running up to a vault style looking door")
     time.sleep(0.5)
-    print ("you climb the ladder and open the door")
+    print("you climb the ladder and open the door")
     time.sleep(0.5)
-    print ("you open the door and climb outside, you immediatly smell something wierd")
-    time.sleep (0.5)
-    print ("you turn around and see that the little cabin is blown up you think \"huh that man outside wasn't joking\"")
+    print("you open the door and climb outside, you immediatly smell something wierd")
     time.sleep(0.5)
-    print ("what should you do now?")
+    print("you turn around and see that the little cabin is blown up you think \"huh that man outside wasn't joking\"")
     time.sleep(0.5)
-    print ("walk to the \"mountain\" after al or explore deeper into the \"forest\"")
+    print("what should you do now?")
+    time.sleep(0.5)
+    print("walk to the \"mountain\" after al or explore deeper into the \"forest\"")
     basement5choise()
-    
+
+
 def basement5choise():
     gq_11 = input("- ")
     if gq_11 == "mountain":
@@ -371,29 +408,33 @@ def basement5choise():
     elif gq_11 == "forest":
         deepforest()
     else:
-        print ("invalid input...")
+        print("invalid input...")
         time.sleep(0.5)
         basement5choise()
-    
+
+
 def end_3():
     time.sleep(0.5)
-    print ("you slowly pick up the beams and try to put them into the hooks")
+    print("you slowly pick up the beams and try to put them into the hooks")
     time.sleep(0.5)
-    print ("right as you lift one of the beams the man kicks in the door")
+    print("right as you lift one of the beams the man kicks in the door")
     time.sleep(0.5)
-    print ("he has a gun...")
+    print("he has a gun...")
     time.sleep(0.5)
-    print ("you can almost see the craze in his raging eyes")
+    print("you can almost see the craze in his raging eyes")
     time.sleep(0.5)
-    print ("you already know this is the end...")
+    print("you already know this is the end...")
     time.sleep(0.5)
-    print ("the man pulls the trigger and the world around you fades away, accompanied with a sharp pain in your chest")
+    print("the man pulls the trigger and the world around you fades away, accompanied with a sharp pain in your chest")
     time.sleep(0.5)
     aorb()
-        
-# a function that serves no purpose it is only called for tests DO NOT REMOVE CAN BE FITAL TO TEST FUNCTIONS   
-def test():
-    print ("it worked")
 
-if __name__ == "__main__": # Starts The Main Function
+
+# a function that serves no purpose it is only called for tests DO NOT REMOVE CAN BE FITAL TO TEST FUNCTIONS
+def test():
+    print("it worked")
+
+
+if __name__ == "__main__":  # Starts The Main Function
     main()
+
